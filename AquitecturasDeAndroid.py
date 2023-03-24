cf79aa1d15575722d18e59e04adee3e2ba76f272
Tinieblas <xngelo_03.h@hotmail.com>
2023-03-24 09:53:09 -0500

    Haciendo la refactorizacion

--- AquitecturasDeAndroid.py
+++ AquitecturasDeAndroid.py
@@ -2,110 +2,11 @@ import os, sys, time, re
 import errno
 from libs.effects  import loading_effect #as effects
 from libs.effects  import imprimir
+from libs.effects  import estructuraDeColores
 
 #path = input("Introduzca la direccion path del proyecto: ")
 path = "java/"
 
-'''
-imprimir("34", "Estructura de carpetas:")
-imprimir("1;38;2;255;215;0", "MyApplication/")
-imprimir("1;38;2;255;215;0", "├── app/")
-imprimir("1;38;2;255;215;0", "│   ├── src/")
-imprimir("1;38;2;255;215;0", "│   │   ├── main/")
-imprimir("38;2;135;206;250", "│   │   │   ├── AndroidManifest.xml")
-imprimir("1;38;2;255;215;0", "│   │   │   ├── java/")
-imprimir("1;38;2;255;215;0", "│   │   │   │   ├── data/")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── database/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── MyDatabase.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── models/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   ├── User.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── Product.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   └── repositories/")
-imprimir("38;2;255;160;122", "│   │   │   │   │       ├── UserRepository.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │       └── ProductRepository.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   ├── di/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   └── AppComponent.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   ├── ui/")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── activities/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   ├── MainActivity.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── ProductDetailActivity.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── ProductAdapter.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── fragments/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   ├── HomeFragment.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── ProductFragment.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── viewmodels/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   ├── UserViewModel.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── ProductViewModel.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   │   ├── dialogs/")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── ConfirmDialog.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   ├── OnProductClickListener.java")
-imprimir("38;2;255;160;122", "│   │   │   │   │   │   └── OnUserClickListener.java")
-imprimir("1;38;2;255;215;0", "│   │   │   │   └── utils/")
-imprimir("38;2;255;160;122", "│   │   │   │   │       └── ImageUtils.java")
-imprimir("38;2;255;160;122", "│   │   │   │   └── MyApplication.java")
-imprimir("1;38;2;255;215;0", "│   │   │   └── res/")
-imprimir("1;38;2;255;215;0", "│   │   │       ├── drawable/")
-imprimir("1;38;2;255;215;0", "│   │   │       ├── layout/")
-imprimir("1;38;2;255;215;0", "│   │   │       ├── mipmap/")
-imprimir("1;38;2;255;215;0", "│   │   │       └── values/")
-imprimir("38;2;135;206;250", "│   │   │           ├── colors.xml")
-'''
-
-estructura = [ 
-    ("MyApplication", None),
-    ("├── app", None),
-    ("│   ├── src", None),
-    ("│   │   ├── main", None),
-    ("│   │   │   ├── AndroidManifest", "xml"),
-    ("│   │   │   ├── java", None),
-    ("│   │   │   │   ├── data", None),
-    ("│   │   │   │   │   ├── database", None),
-    ("│   │   │   │   │   │   └── MyDatabase", "java"),
-    ("│   │   │   │   │   ├── models", None),
-    ("│   │   │   │   │   │   ├── User", "java"),
-    ("│   │   │   │   │   │   └── Product", "java"),
-    ("│   │   │   │   │   └── repositories", None),
-    ("│   │   │   │   │       ├── UserRepository", "java"),
-    ("│   │   │   │   │       └── ProductRepository", "java"),
-    ("│   │   │   │   ├── di", None),
-    ("│   │   │   │   │   └── AppComponent", "java"),
-    ("│   │   │   │   ├── ui", None),
-    ("│   │   │   │   │   ├── activities", None),
-    ("│   │   │   │   │   │   ├── MainActivity", "java"),
-    ("│   │   │   │   │   │   └── ProductDetailActivity", "java"),
-    ("│   │   │   │   │   │   └── ProductAdapter", "java"),
-    ("│   │   │   │   │   ├── fragments", None),
-    ("│   │   │   │   │   │   ├── HomeFragment", "java"),
-    ("│   │   │   │   │   │   └── ProductFragment", "java"),
-    ("│   │   │   │   │   ├── viewmodels", None),
-    ("│   │   │   │   │   │   ├── UserViewModel", "java"),
-    ("│   │   │   │   │   │   └── ProductViewModel", "java"),
-    ("│   │   │   │   │   ├── dialogs", None),
-    ("│   │   │   │   │   │   ├── ConfirmDialog", "java"),
-    ("│   │   │   │   │   │   ├── OnProductClickListener", "java"),
-    ("│   │   │   │   │   │   └── OnUserClickListener", "java"),
-    ("│   │   │   │   └── utils", None),
-    ("│   │   │   │   │       └── ImageUtils", "java"),
-    ("│   │   │   │   └── MyApplication", "java"),
-    ("│   │   │   └── res", None),
-    ("│   │   │       ├── drawable", None),
-    ("│   │   │       ├── layout", None),
-    ("│   │   │       ├── mipmap", None),
-    ("│   │   │       └── values", None),
-    ("│   │   │           ├── colors", "xml"),
-    ("│   │   │           ├── strings", "xml"),
-    ("│   │   │           └── styles", "xml"),
-    ("│   │   └── test", None),
-    ("│   └── build", "gradle"),
-    ("├── Gradle Scripts", None),
-    ("│   ├── build", "gradle"),
-    ("│   ├── gradle", None),
-    ("│   └── settings", "gradle"),
-    ("├── build", None),
-    ("├── README", "md"),
-    ("├── LICENSE", "ninguno"),
-    ("└── ", "gitignore")
-]
 ruta_archivos = [
     ("data/", None),
     ("data/database", None),
@@ -134,54 +35,41 @@ ruta_archivos = [
     ("ui/dialogs/OnProductClickListener", "java"),
     ("ui/dialogs/OnUserClickListener", "java"),
     ("utils/", None),
-    ("utils/ImageUtils", "java"),
-    ("MyApplication", "java")
+    ("utils/ImageUtils", "java")
+    #("MyApplication", "java")
 ]
 
-def estructuraDeColores():
-    for nombre, extension in estructura:
-        if extension == 'java':
-            imprimir('38;2;255;160;122', f"{nombre}.{extension}")
-        elif extension == 'xml':
-           imprimir('38;2;135;206;250', f"{nombre}.{extension}")
-        elif extension == 'gradle':
-            imprimir("33", f"{nombre}.{extension}")
-        elif extension == 'md':
-            imprimir("38;2;135;206;250", f"{nombre}.{extension}")
-        elif extension == 'ninguno':
-            imprimir("38;2;255;160;122", f"{nombre}.{extension}")
-        elif extension == 'gitignore':
-            imprimir("38;2;255;215;0", f"{nombre}.{extension}")
-
-    else:
-        imprimir('1;38;2;255;215;0', nombre + '/')
-
+estructuraDeColores()
 
 # Ruta base donde se crearan los archivos y carpetas
 path = "java/"
+def generar_archivos_directorios():
 
-for folder, extension in ruta_archivos:
-    folder_path = os.path.join(path, folder)
+    nombre_package = input("package com.nombre.nombre.ui.ativities\nIntroduzca el nombre de su package: ")
+    for folder, extension in ruta_archivos:
+        folder_path = os.path.join(path, folder)
     
-    # Verificar si la carpeta ya existe
-    if os.path.isdir(folder_path):
-        print(f"La carpeta {folder_path} ya existe")
-    else:
-        os.makedirs(folder_path)
-        print(f"Se creó la carpeta {folder_path}")
-    
-    # Crear archivo si se especificó una extensión
-    if extension:
-        file_path = os.path.join(folder_path, folder.split("/")[-1] + "." + extension)
-        
-        # Verificar si el archivo ya existe
-        if os.path.isfile(file_path):
-            print(f"El archivo {file_path} ya existe")
+        # Verificar si la carpeta ya existe
+        if os.path.isdir(folder_path):
+            print(f"La carpeta {folder_path} ya existe")
         else:
-            open(file_path, "w").close()
+            os.makedirs(folder_path)
+            print(f"Se creó la carpeta {folder_path}")
+    
+        # Crear archivo si se especificó una extensión
+        if extension:
+            file_path = os.path.join(folder_path, folder.split("/")[-1] + "." + extension)
+        
+            # Verificar si el archivo ya existe
+            if os.path.isfile(file_path):
+                print(f"El archivo {file_path} ya existe")
+            else:
+                file = open(file_path, "w")#.close()
+                #file.write("package com."+ nombre_package + ".ui.fragments;\n \n")
+                print(file_path)
             print(f"Se creó el archivo {file_path}")
 
-
+generar_archivos_directorios()
 #loading_effect(100)
 
 '''createFileJava("data/models", "User", ".java")'''
--- libs/effects.py
+++ libs/effects.py
@@ -25,23 +25,80 @@ def loading_effect(time):
   print("The process has been completed satisfactorily!")
 
 
+    
 #CON ESO SALE LOS COLORES
-'''
 def estructuraDeColores():
-    for nombre, extension in estructura:
-        if extension == 'java':
-            imprimir('38;2;255;160;122', f"{nombre}.{extension}")
-        elif extension == 'xml':
-           imprimir('38;2;135;206;250', f"{nombre}.{extension}")
-        elif extension == 'gradle':
-            imprimir("33", f"{nombre}.{extension}")
-        elif extension == 'md':
-            imprimir("38;2;135;206;250", f"{nombre}.{extension}")
-        elif extension == 'ninguno':
-            imprimir("38;2;255;160;122", f"{nombre}.{extension}")
-        elif extension == 'gitignore':
-            imprimir("38;2;255;215;0", f"{nombre}.{extension}")
+   #imprimir("34", "Estructura de carpetas:")
+   estructura = [ ("MyApplication", None),
+    ("├── app", None),
+    ("│   ├── src", None),
+    ("│   │   ├── main", None),
+    ("│   │   │   ├── AndroidManifest", "xml"),
+    ("│   │   │   ├── java", None),
+    ("│   │   │   │   ├── data", None),
+    ("│   │   │   │   │   ├── database", None),
+    ("│   │   │   │   │   │   └── MyDatabase", "java"),
+    ("│   │   │   │   │   ├── models", None),
+    ("│   │   │   │   │   │   ├── User", "java"),
+    ("│   │   │   │   │   │   └── Product", "java"),
+    ("│   │   │   │   │   └── repositories", None),
+    ("│   │   │   │   │       ├── UserRepository", "java"),
+    ("│   │   │   │   │       └── ProductRepository", "java"),
+    ("│   │   │   │   ├── di", None),
+    ("│   │   │   │   │   └── AppComponent", "java"),
+    ("│   │   │   │   ├── ui", None),
+    ("│   │   │   │   │   ├── activities", None),
+    ("│   │   │   │   │   │   ├── MainActivity", "java"),
+    ("│   │   │   │   │   │   └── ProductDetailActivity", "java"),
+    ("│   │   │   │   │   │   └── ProductAdapter", "java"),
+    ("│   │   │   │   │   ├── fragments", None),
+    ("│   │   │   │   │   │   ├── HomeFragment", "java"),
+    ("│   │   │   │   │   │   └── ProductFragment", "java"),
+    ("│   │   │   │   │   ├── viewmodels", None),
+    ("│   │   │   │   │   │   ├── UserViewModel", "java"),
+    ("│   │   │   │   │   │   └── ProductViewModel", "java"),
+    ("│   │   │   │   │   ├── dialogs", None),
+    ("│   │   │   │   │   │   ├── ConfirmDialog", "java"),
+    ("│   │   │   │   │   │   ├── OnProductClickListener", "java"),
+    ("│   │   │   │   │   │   └── OnUserClickListener", "java"),
+    ("│   │   │   │   └── utils", None),
+    ("│   │   │   │   │       └── ImageUtils", "java"),
+    ("│   │   │   │   └── MyApplication", "java"),
+    ("│   │   │   └── res", None),
+    ("│   │   │       ├── drawable", None),
+    ("│   │   │       ├── layout", None),
+    ("│   │   │       ├── mipmap", None),
+    ("│   │   │       └── values", None),
+    ("│   │   │           ├── colors", "xml"),
+    ("│   │   │           ├── strings", "xml"),
+    ("│   │   │           └── styles", "xml"),
+    ("│   │   └── test", None),
+    ("│   └── build", "gradle"),
+    ("├── Gradle Scripts", None),
+    ("│   ├── build", "gradle"),
+    ("│   ├── gradle", None),
+    ("│   └── settings", "gradle"),
+    ("├── build", None),
+    ("├── REAaaaDME", "md"),
+    ("├── LICENSE", "ninguno"),
+    ("└── ", "gitignore")
+]
+   
+   for nombre, extension in estructura:
+    if extension == 'java':
+        imprimir('38;2;255;160;122', f"{nombre}.{extension}")
+    elif extension == 'xml':
+       imprimir('38;2;135;206;250', f"{nombre}.{extension}")
+    elif extension == 'gradle':
+        imprimir("33", f"{nombre}.{extension}")
+    elif extension == 'md':
+        imprimir("38;2;135;206;250", f"{nombre}.{extension}")
+    elif extension == 'ninguno':
+        imprimir("38;2;255;160;122", f"{nombre}.{extension}")
+    elif extension == 'gitignore':
+        imprimir("38;2;255;215;0", f"{nombre}.{extension}")
 
     else:
         imprimir('1;38;2;255;215;0', nombre + '/')
-'''
+
+
